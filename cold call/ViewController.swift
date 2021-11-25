c2cd29d658f18eaa195138e2d189c513763ac383
faisal alfariheedi <falfariheedi@gmail.com>
2021-11-25 14:47:31 +0300

    fin v2

--- cold call/ViewController.swift
+++ cold call/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var names: UILabel!
+    @IBOutlet weak var num: UILabel!
+    var a:Int = 0
     var name=["khalid","ahmed","onion","burger","batman"]
    
     override func viewDidLoad() {
@@ -17,7 +19,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func callonclick(_ sender: UIButton) {
-        names.text=name[Int.random(in: 0..<name.count)]
+        a=Int.random(in: 0..<name.count)
+        if(a+1>0){
+            num.textColor=UIColor.red
+        }
+        if(a+1>2){
+            num.textColor=UIColor.orange
+        }
+        if(a+1>4){
+            num.textColor=UIColor.green
+        }
+        num.text="\(a)"
+        names.text=name[a]
         
         
     }
