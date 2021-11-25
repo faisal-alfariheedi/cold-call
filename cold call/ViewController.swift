8ba69e6e3c3ba9a0909038defadf2724d322dab9
faisal alfariheedi <falfariheedi@gmail.com>
2021-11-25 13:36:47 +0300

    fin

--- cold call/ViewController.swift
+++ cold call/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var names: UILabel!
+    var name=["khalid","ahmed","onion","burger","batman"]
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        names.text="Ready?"
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func callonclick(_ sender: UIButton) {
+        names.text=name[Int.random(in: 0..<name.count)]
+        
+        
+    }
+    
 
 }
 
